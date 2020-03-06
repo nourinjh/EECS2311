8d28343ecd8de4e2629913d6dd03af926e96123b
AndrewHocking <andrew.hocking@hotmail.ca>
2020-03-06 02:57:59 -0500

    Initial implementaion of draggable text objects
    - Created DraggableItem class
    - Items can be dragged from list into diagram *anywhere*
    - On macOS, menu bar now appears in native menu bar
    - Keyboard shortcuts are now platform agnostic

--- Venn/src/main/java/application/Controller.java
+++ Venn/src/main/java/application/Controller.java
@@ -6,6 +6,12 @@
 // 5. Click finish
 // 6. Pray
 
+/**
+ * 
+ * @author Andrew Hocking
+ * 
+ */
+
 package application;
 
 import java.awt.Rectangle;
@@ -29,6 +35,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
@@ -49,10 +56,19 @@ import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -162,6 +178,101 @@ public class Controller {
 	Alert a = new Alert(AlertType.NONE);
 
 	private String fileTitle = null;
+	
+	class DraggableItem extends StackPane {
+		private Label text = new Label();
+
+		DraggableItem(double x, double y) {
+			relocate(x - 5.0D, y - 5.0D);
+			getChildren().add(this.text);
+			text.setTextFill(Color.WHITE);
+			setBorder(new Border(new BorderStroke(Color.DEEPSKYBLUE, BorderStrokeStyle.NONE, new CornerRadii(1), new BorderWidths(5), new Insets(2, 2, 2, 2))));
+			requestFocus();
+
+			this.text.focusedProperty().addListener((observable, hadFocus, hasFocus) -> {
+				if (!hasFocus.booleanValue() && getParent() != null && getParent() instanceof Pane
+						&& (this.text.getText() == null || this.text.getText().trim().isEmpty())) {
+					((Pane) getParent()).getChildren().remove(this);
+				}
+			});
+			
+			this.setOnMouseClicked(mouseEvent -> {
+				requestFocus();
+				setBorder(new Border(new BorderStroke(Color.DEEPSKYBLUE, BorderStrokeStyle.SOLID, new CornerRadii(1), new BorderWidths(5), new Insets(2, 2, 2, 2))));
+			});
+			
+			this.focusedProperty().addListener((observable, hadFocus, hasFocus) -> {
+				if (!hasFocus.booleanValue()) {
+					setBorder(new Border(new BorderStroke(Color.DEEPSKYBLUE, BorderStrokeStyle.NONE, new CornerRadii(1), new BorderWidths(5), new Insets(2, 2, 2, 2))));
+				}
+			});
+
+			enableDrag();
+		}
+
+		public DraggableItem(double d, double e, String text) {
+			this(d, e);
+			this.text.setText(text);
+		}
+		
+		public Label getLabel() {
+			return this.text;
+		}
+		
+		public String getText() {
+			return this.text.getText();
+		}
+
+		private void enableDrag() {
+			Delta dragDelta = new Delta();
+			setOnMousePressed(mouseEvent -> {
+				toFront();
+
+				dragDelta.x = mouseEvent.getX();
+				dragDelta.y = mouseEvent.getY();
+				getScene().setCursor(Cursor.CLOSED_HAND);
+			});
+			setOnMouseReleased(mouseEvent -> {
+				getScene().setCursor(Cursor.HAND);
+				if (this.text.intersects(circleLeft.getBoundsInParent()) && this.text.intersects(circleRight.getBoundsInParent())) {
+					this.text.setTextFill(Color.BLUE);
+				}
+				else if (this.text.intersects(circleLeft.getBoundsInParent())) {
+					this.text.setTextFill(Color.RED);
+				}
+				else if (this.text.intersects(circleRight.getBoundsInParent())) {
+					this.text.setTextFill(Color.GREEN);
+				}
+				else {
+					this.text.setTextFill(Color.WHITE);
+				}
+			});				
+			setOnMouseDragged(mouseEvent -> {
+				double newX = getLayoutX() + mouseEvent.getX() - dragDelta.x;
+				if (newX > floatingMenu.getLayoutX() + floatingMenu.getWidth() && newX < getScene().getWidth() - getWidth()) {
+					setLayoutX(newX);
+				}
+				double newY = getLayoutY() + mouseEvent.getY() - dragDelta.y;
+				if (newY > circleLeftTitle.getLayoutY() + 3*circleLeftTitle.getHeight() + getHeight() && newY < getScene().getHeight() - getHeight()) {
+					setLayoutY(newY);
+				}
+			});
+			setOnMouseEntered(mouseEvent -> {
+				if (!mouseEvent.isPrimaryButtonDown()) {
+					getScene().setCursor(Cursor.HAND);
+				}
+			});
+			setOnMouseExited(mouseEvent -> {
+				if (!mouseEvent.isPrimaryButtonDown())
+					getScene().setCursor(Cursor.DEFAULT);
+			});
+		}
+
+		private class Delta {
+			double x;
+			double y;
+		}
+	}
 
 	@FXML
 	void addItemToList() {
@@ -185,7 +296,7 @@ public class Controller {
 
 	@FXML
 	void dragFromItemsList() {
-		Dragboard dragBoard = itemsList.startDragAndDrop(TransferMode.MOVE);
+		Dragboard dragBoard = itemsList.startDragAndDrop(TransferMode.ANY);
 		ClipboardContent content = new ClipboardContent();
 		content.putString(itemsList.getSelectionModel().getSelectedItem());
 		dragBoard.setContent(content);
@@ -232,7 +343,7 @@ public class Controller {
 
 	@FXML
 	void dragOverList(DragEvent event) {
-		event.acceptTransferModes(TransferMode.MOVE);
+		event.acceptTransferModes(TransferMode.ANY);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -249,7 +360,7 @@ public class Controller {
 
 	@FXML
 	void dragFromCircleRightItemsList() {
-		Dragboard dragBoard = circleRightItemsList.startDragAndDrop(TransferMode.MOVE);
+		Dragboard dragBoard = circleRightItemsList.startDragAndDrop(TransferMode.ANY);
 		ClipboardContent content = new ClipboardContent();
 		content.putString(circleRightItemsList.getSelectionModel().getSelectedItem());
 		dragBoard.setContent(content);
@@ -279,7 +390,7 @@ public class Controller {
 
 	@FXML
 	void dragFromCircleLeftItemsList() {
-		Dragboard dragBoard = circleLeftItemsList.startDragAndDrop(TransferMode.MOVE);
+		Dragboard dragBoard = circleLeftItemsList.startDragAndDrop(TransferMode.ANY);
 		ClipboardContent content = new ClipboardContent();
 		content.putString(circleLeftItemsList.getSelectionModel().getSelectedItem());
 		dragBoard.setContent(content);
@@ -309,7 +420,7 @@ public class Controller {
 
 	@FXML
 	void dragFromBothItemsList() {
-		Dragboard dragBoard = bothItemsList.startDragAndDrop(TransferMode.MOVE);
+		Dragboard dragBoard = bothItemsList.startDragAndDrop(TransferMode.ANY);
 		ClipboardContent content = new ClipboardContent();
 		content.putString(bothItemsList.getSelectionModel().getSelectedItem());
 		dragBoard.setContent(content);
@@ -755,11 +866,13 @@ public class Controller {
 //			a.setTitle("Error");
 //			a.show();
 //		}
-		a.setAlertType(AlertType.INFORMATION);
-		a.setHeaderText("This feature is not yet available");
-		a.setContentText("Adding images to diagrams will be available in a future release");
-		a.setTitle("Feature not yet available");
-		a.show();
+//		a.setAlertType(AlertType.INFORMATION);
+//		a.setHeaderText("This feature is not yet available");
+//		a.setContentText("Adding images to diagrams will be available in a future release");
+//		a.setTitle("Feature not yet available");
+//		a.show();
+		DraggableItem a = new DraggableItem(frameRect.getLayoutX(), frameRect.getLayoutY(), "nice");
+		pane.getChildren().add(a);
 	}
 
 	@FXML
@@ -782,18 +895,35 @@ public class Controller {
 
 	@FXML
 	void removeFocus() {
-		pane.requestFocus();
+//		pane.requestFocus();
 		itemsList.getSelectionModel().clearSelection();
 		circleLeftItemsList.getSelectionModel().clearSelection();
 		bothItemsList.getSelectionModel().clearSelection();
 		circleRightItemsList.getSelectionModel().clearSelection();
 	}
+	
+	@SuppressWarnings("unchecked")
+	@FXML
+	void dropItem(DragEvent event) {
+		String item = event.getDragboard().getString();
+		DraggableItem a = new DraggableItem(event.getSceneX(), event.getSceneY(), item);
+		pane.getChildren().add(a);
+		((ListView<String>) event.getGestureSource()).getItems().remove(item);
+		event.setDropCompleted(true);
+		event.consume();
+	}
 
 	// TODO:
+	// - Customize colours of dragged items
+	// - Categorize dragged items
+	// - Detail view for dragged items
+	// - Implement saving and loading dragged items
+	// - Enable CSV file imports
 	// - Add right click menus
 	// - Responsive design
 	// - Import an image
-	// - Drag items wherever
+	// - Add tooltips
+	// - Quick-action toolbar with icon buttons
 
 	// This method is called by the FXMLLoader when initialization is complete
 	@FXML
@@ -802,13 +932,15 @@ public class Controller {
 			@Override
 			public void run() {
 				pane.requestFocus();
-//				String os = System.getProperty("os.name");
-//				if (os != null && os.startsWith("Mac"))
-//					menuBar.useSystemMenuBarProperty().set(true);
+				String os = System.getProperty("os.name");
+				if (os != null && os.startsWith("Mac"))
+					menuBar.useSystemMenuBarProperty().set(true);
 				doTheNew();
 				settingsPane.setExpanded(false);
 				leftSizeField.setAlignment(Pos.CENTER);
 				rightSizeField.setAlignment(Pos.CENTER);
+//				frameRect.getChildren().add(new javafx.scene.shape.Rectangle((int)(circleLeft.getBoundsInParent().getMinX()), (int)(circleLeft.getBoundsInParent().getMinY()), (int)(2*circleLeft.getRadius()), (int)(2*circleLeft.getRadius())));
+//				frameRect.getChildren().add(new javafx.scene.shape.Rectangle((int)(circleRight.getBoundsInParent().getMinX()), (int)(circleRight.getBoundsInParent().getMinY()), (int)(2*circleRight.getRadius()), (int)(2*circleRight.getRadius())));
 			}
 		});
 
